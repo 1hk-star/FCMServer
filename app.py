4f1ac06e5dd916a898c160ebe9d3fb1d5cf41a11
Khabib Murtuzaaliev <khabib.murtuzaaliev@gmail.com>
2017-03-10 07:52:50 +0300

    Run on port argument

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import argparse
+
 import tornado.wsgi
 import tornado.httpserver
 import tornado.ioloop
@@ -13,13 +15,19 @@ def save_key(key):
     return 'Key saved'
 
 
-if __name__ == '__main__':
+def run(port=8080):
     appsched.start()
     http_server = tornado.httpserver.HTTPServer(
         tornado.wsgi.WSGIContainer(app)
     )
     http_server.listen(8080, "0.0.0.0")
-    print("Tornado server starting on port {}".format(8080))
+    print("Tornado server starting on port {}".format(port))
     tornado.ioloop.IOLoop.instance().start()
 
-
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Run a task')
+    parser.add_argument('--port', dest='port',
+                        help='Port to run on',
+                        default=8080, type=int)
+    args = parser.parse_args()
+    run(args.port)
